840e756463d16fe15dfe8c1248f920bff6983a61
anubhootipandey <anubhootipandey05@gmail.com>
2024-03-22 11:13:25 +0530

    Pattern-problems

---
+++ 00_HomeWorkProblems/SolidRhombusPattern.java
@@ -0,0 +1,14 @@
+public class SolidRhombusPattern {
+    public static void main(String[] args){
+        int n = 4;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");//it prints the space before star 
+            }
+            for(int j=1; j<=n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
